ee1219d466ee70d98cf09908be999a5d2e2a0cff
Sindre Wilting <sindrewilting@gmail.com>
2020-09-07 17:31:09 +0200

    Expanded upon sample

--- Samples/Basic/Program.cs
+++ Samples/Basic/Program.cs
@@ -11,7 +11,10 @@ namespace Basic
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var client = Client.ForMicroservice("e59582cb-e51d-408e-8f2d-b5e47ed08d65").Build();
+            var client = Client.ForMicroservice("e59582cb-e51d-408e-8f2d-b5e47ed08d65")
+                .WithEventTypes(artifactsBuilder =>
+                    artifactsBuilder.Associate<MyEvent>("f42529b3-d980-4b55-8fbe-65101a6141a3"))
+                .Build();
             client.ExecutionContextManager.ForTenant("546fcbec-8167-41a0-b865-1d881e6efe9e");
         }
     }
