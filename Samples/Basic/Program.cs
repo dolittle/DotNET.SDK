b9415b5403eb35844483dce8e046de4d693440a8
Sindre Wilting <sindrewilting@gmail.com>
2020-09-08 15:41:20 +0200

    Change callback name

--- Samples/Basic/Program.cs
+++ Samples/Basic/Program.cs
@@ -12,8 +12,8 @@ namespace Basic
         {
             Console.WriteLine("Hello World!");
             var client = Client.ForMicroservice("e59582cb-e51d-408e-8f2d-b5e47ed08d65")
-                .WithEventTypes(artifactsBuilder =>
-                    artifactsBuilder.Associate<MyEvent>("f42529b3-d980-4b55-8fbe-65101a6141a3"))
+                .WithEventTypes(eventTypes =>
+                    eventTypes.Associate<MyEvent>("f42529b3-d980-4b55-8fbe-65101a6141a3"))
                 .Build();
             client.ExecutionContextManager.ForTenant("546fcbec-8167-41a0-b865-1d881e6efe9e");
         }
