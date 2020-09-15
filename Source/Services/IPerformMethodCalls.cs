fdd2afb474effa742879ec264112d65096602fbd
Joel Hoisko <joel.hoisko@gmail.com>
2020-09-15 10:32:47 +0200

    Fix imports

--- Source/Services/IPerformMethodCalls.cs
+++ Source/Services/IPerformMethodCalls.cs
@@ -2,8 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Google.Protobuf;
+using Grpc.Core;
 
 namespace Dolittle.SDK.Services
 {
