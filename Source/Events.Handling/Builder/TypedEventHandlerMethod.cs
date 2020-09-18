539ccdb64d4a345a93a1d48c721d254145c9e03f
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2020-09-18 09:15:25 +0200

    Update Source/Events.Handling/Builder/TypedEventHandlerMethod.cs
    Co-authored-by: Jakob Høgenes <1014990+jakhog@users.noreply.github.com>

--- Source/Events.Handling/Builder/TypedEventHandlerMethod.cs
+++ Source/Events.Handling/Builder/TypedEventHandlerMethod.cs
@@ -27,18 +27,10 @@ namespace Dolittle.SDK.Events.Handling.Builder
             _method = method;
         }
 
-        /// <summary>
-        /// Invokes the event handler method.
-        /// </summary>
-        /// <param name="event">The <typeparamref name="T"/> event.</param>
-        /// <param name="context">The <see cref="EventContext" />.</param>
-        /// <returns>A <see cref="Task" /> representing the asynchronous action.</returns>
-        public Task Invoke(T @event, EventContext context) => _method(@event, context);
-
         /// <inheritdoc/>
-        Task IEventHandlerMethod.Invoke(object @event, EventContext context)
+        Task Invoke(object @event, EventContext context)
         {
-            if (@event is T) return Invoke(@event as T, context);
+            if (@event is T typedEvent) return _method(typedEvent, context);
             throw new TypedEventHandlerMethodInvokedOnEventOfWrongType(_eventHandlerId, typeof(T), @event.GetType());
         }
     }
