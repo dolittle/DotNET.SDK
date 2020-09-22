a7c276e683820f535ad6fb76a2719d9c8b2ae5a6
Sindre Wilting <sindrewilting@gmail.com>
2020-09-22 13:48:47 +0200

    Add specs for concepts ffrom Fundamentals

---
+++ Specifications/Concepts/for_Value/when_equating_a_single_instance.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Machine.Specifications;
+
+namespace Dolittle.SDK.Concepts.for_Value
+{
+    [Subject(typeof(Value<>))]
+    public class when_equating_a_single_instance : given.value_objects
+    {
+        static bool is_equal;
+
+        Because of = () => is_equal = first_value.Equals(first_value);
+
+        It should_be_equal = () => is_equal.ShouldBeTrue();
+    }
+}
