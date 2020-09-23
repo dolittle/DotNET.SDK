9f7b5f7d6a06c185e5c39e014878cca737a5d025
Sindre Wilting <sindrewilting@gmail.com>
2020-09-23 08:17:50 +0200

    Event should not extend Value

--- Specifications/Events.Handling/Internal/for_EventHandlerProcessor/given/some_event.cs
+++ Specifications/Events.Handling/Internal/for_EventHandlerProcessor/given/some_event.cs
@@ -1,11 +1,9 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Dolittle.SDK.Concepts;
-
 namespace Dolittle.SDK.Events.Handling.Internal.for_EventHandlerProcessor.given
 {
-    public class some_event : Value<some_event>
+    public class some_event
     {
         public string some_string { get; set; }
     }
