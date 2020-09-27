4836176065f5af06fa7c1c95e7c8df0fe2e56cf3
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-09-27 18:04:37 +0200

    Specs for EventHorizon builders with callbacks

--- Specifications/EventHorizon/for_SubscriptionsBuilderForConsumerTenant/when_building_with_callbacks.cs
+++ Specifications/EventHorizon/for_SubscriptionsBuilderForConsumerTenant/when_building_with_callbacks.cs
@@ -6,7 +6,7 @@ using Machine.Specifications;
 using Moq;
 using It = Machine.Specifications.It;
 
-namespace Dolittle.SDK.EventHorizon.for_SubscriptionsBuilder
+namespace Dolittle.SDK.EventHorizon.for_SubscriptionsBuilderForConsumerTenant
 {
     public class when_building_with_callbacks : given.event_horizons_with_observable_responses
     {
@@ -14,7 +14,7 @@ namespace Dolittle.SDK.EventHorizon.for_SubscriptionsBuilder
         static Mock<SubscriptionFailed> failed_handler;
         static Mock<SubscriptionCompleted> completed_handler;
 
-        static SubscriptionsBuilder builder;
+        static SubscriptionsBuilderForConsumerTenant builder;
 
         Establish context = () =>
         {
@@ -22,7 +22,7 @@ namespace Dolittle.SDK.EventHorizon.for_SubscriptionsBuilder
             failed_handler = new Mock<SubscriptionFailed>();
             completed_handler = new Mock<SubscriptionCompleted>();
 
-            builder = new SubscriptionsBuilder();
+            builder = new SubscriptionsBuilderForConsumerTenant(tenant_two);
 
             builder.OnSuccess(succeeded_handler.Object);
             builder.OnFailure(failed_handler.Object);
@@ -31,16 +31,12 @@ namespace Dolittle.SDK.EventHorizon.for_SubscriptionsBuilder
 
         Because of = () => builder.BuildAndSubscribe(event_horizons, CancellationToken.None);
 
-        It should_call_the_succeded_handler_with_subscription_tenant_one_success = () => succeeded_handler.Verify(_ => _.Invoke(subscription_tenant_one, consent_id), Times.Once());
         It should_call_the_succeded_handler_with_subscription_one_tenant_two_success = () => succeeded_handler.Verify(_ => _.Invoke(subscription_one_tenant_two, consent_id), Times.Once());
         It should_call_the_succeded_handler_with_subscription_two_tenant_two_success = () => succeeded_handler.Verify(_ => _.Invoke(subscription_two_tenant_two, consent_id), Times.Once());
 
-        It should_call_the_failed_handler_with_subscription_tenant_one_failure = () => failed_handler.Verify(_ => _.Invoke(subscription_tenant_one, failure), Times.Once());
         It should_call_the_failed_handler_with_subscription_one_tenant_two_failure = () => failed_handler.Verify(_ => _.Invoke(subscription_one_tenant_two, failure), Times.Once());
         It should_call_the_failed_handler_with_subscription_two_tenant_two_failure = () => failed_handler.Verify(_ => _.Invoke(subscription_two_tenant_two, failure), Times.Once());
 
-        It should_call_the_completed_handler_with_subscription_tenant_one_success = () => completed_handler.Verify(_ => _.Invoke(response_subscription_tenant_one_success), Times.Once());
-        It should_call_the_completed_handler_with_subscription_tenant_one_failure = () => completed_handler.Verify(_ => _.Invoke(response_subscription_tenant_one_failure), Times.Once());
         It should_call_the_completed_handler_with_subscription_one_tenant_two_success = () => completed_handler.Verify(_ => _.Invoke(response_subscription_one_tenant_two_success), Times.Once());
         It should_call_the_completed_handler_with_subscription_one_tenant_two_failure = () => completed_handler.Verify(_ => _.Invoke(response_subscription_one_tenant_two_failure), Times.Once());
         It should_call_the_completed_handler_with_subscription_two_tenant_two_success = () => completed_handler.Verify(_ => _.Invoke(response_subscription_two_tenant_two_success), Times.Once());
