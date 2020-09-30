237f3d0ae4b7f31eae1994ea2266f57d7a86fe86
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-09-30 21:42:01 +0200

    Retry extension methods

---
+++ Source/Resilience/CancellationTokenObservable.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading;
+
+namespace Dolittle.SDK.Resilience
+{
+    /// <summary>
+    /// Represents an <see cref="IObservable{T}"/> that completes when a <see cref="CancellationToken"/> is cancelled.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the sequence.</typeparam>
+    public class CancellationTokenObservable<T> : IObservable<T>
+    {
+        readonly CancellationToken _token;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CancellationTokenObservable{T}"/> class.
+        /// </summary>
+        /// <param name="token">The <see cref="CancellationToken"/> to wait for.</param>
+        public CancellationTokenObservable(CancellationToken token)
+        {
+            _token = token;
+        }
+
+        /// <inheritdoc/>
+        public IDisposable Subscribe(IObserver<T> observer)
+            => _token.Register(() => observer.OnCompleted());
+    }
+}
---
+++ Source/Resilience/ObservableExtensions.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace Dolittle.SDK.Resilience
+{
+    /// <summary>
+    /// Extension methods for <see cref="IObservable{T}"/>.
+    /// </summary>
+    public static class ObservableExtensions
+    {
+        /// <summary>
+        /// Repeats the source observable with the provided <see cref="RetryPolicy"/>.
+        /// </summary>
+        /// <param name="source">Observable sequence to repeat while the policy dictates so or until it successfully terminates.</param>
+        /// <param name="policy">The retry policy to apply.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> that can be used to cancel the retries.</param>
+        /// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
+        /// <returns>>An observable sequence producing the elements of the given sequence repeatedly while the policy dictates so or until it terminates successfully.</returns>
+        public static IObservable<T> RetryWithPolicy<T>(this IObservable<T> source, RetryPolicy policy, CancellationToken cancellationToken)
+            => source.RetryWhen((exceptions) => policy(exceptions).TakeUntil(cancellationToken.ToObservable<Unit>()));
+    }
+}
