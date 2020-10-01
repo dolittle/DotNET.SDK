cbd84432507f7806076c6a895e65f1737d7a9d5a
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-10-01 20:06:57 +0200

    Fix EventStore

--- Source/Events/IEventStore.cs
+++ Source/Events/IEventStore.cs
@@ -12,25 +12,6 @@ namespace Dolittle.SDK.Events
     public interface IEventStore
     {
         /// <summary>
-        /// Commits <see cref="UncommittedEvents" />.
-        /// </summary>
-        /// <param name="uncommittedEvents">The <see cref="UncommittedEvents" />.</param>
-        /// <param name="cancellationToken">Token that can be used to cancel this operation.</param>
-        /// <returns>A <see cref="Task"/> that, when resolved, returns the <see cref="CommittedEvents" />.</returns>
-        /// <remarks>
-        /// Cancelling this operation does not roll back the commit transaction if the events have already been written to the Event Store.
-        /// </remarks>
-        Task<CommitEventsResult> Commit(UncommittedEvents uncommittedEvents, CancellationToken cancellationToken = default);
-
-        /// <summary>
-        /// Commits an <see cref="UncommittedEvent" />.
-        /// </summary>
-        /// <param name="uncommittedEvent">The <see cref="UncommittedEvent" />.</param>
-        /// <param name="cancellationToken">Token that can be used to cancel this operation.</param>
-        /// <returns>A <see cref="Task"/> that, when resolved, returns the <see cref="CommittedEvent" />.</returns>
-        Task<CommitEventsResult> Commit(UncommittedEvent uncommittedEvent, CancellationToken cancellationToken = default);
-
-        /// <summary>
         /// Commits a single Event with the given content.
         /// </summary>
         /// <param name="content">The content of the Event.</param>
@@ -50,6 +31,16 @@ namespace Dolittle.SDK.Events
         Task<CommitEventsResult> Commit(object content, EventSourceId eventSourceId, EventType eventType, CancellationToken cancellationToken = default);
 
         /// <summary>
+        /// Commits a single Event with the given content.
+        /// </summary>
+        /// <param name="content">The content of the Event.</param>
+        /// <param name="eventSourceId">The <see cref="EventSourceId" />.</param>
+        /// <param name="eventTypeId">The <see cref="EventTypeId"/> the Event is associated with.</param>
+        /// <param name="cancellationToken">Token that can be used to cancel this operation.</param>
+        /// <returns>A <see cref="Task"/> that, when resolved, returns the <see cref="CommittedEvent" />.</returns>
+        Task<CommitEventsResult> Commit(object content, EventSourceId eventSourceId, EventTypeId eventTypeId, CancellationToken cancellationToken = default);
+
+        /// <summary>
         /// Commits a single public Event with the given content.
         /// </summary>
         /// <param name="content">The content of the Event.</param>
@@ -67,5 +58,34 @@ namespace Dolittle.SDK.Events
         /// <param name="cancellationToken">Token that can be used to cancel this operation.</param>
         /// <returns>A <see cref="Task"/> that, when resolved, returns the <see cref="CommittedEvent" />.</returns>
         Task<CommitEventsResult> CommitPublic(object content, EventSourceId eventSourceId, EventType eventType, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Commits a single public Event with the given content.
+        /// </summary>
+        /// <param name="content">The content of the Event.</param>
+        /// <param name="eventSourceId">The <see cref="EventSourceId" />.</param>
+        /// <param name="eventTypeId">The <see cref="EventTypeId"/> the Event is associated with.</param>
+        /// <param name="cancellationToken">Token that can be used to cancel this operation.</param>
+        /// <returns>A <see cref="Task"/> that, when resolved, returns the <see cref="CommittedEvent" />.</returns>
+        Task<CommitEventsResult> CommitPublic(object content, EventSourceId eventSourceId, EventTypeId eventTypeId, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Commits an <see cref="UncommittedEvent" />.
+        /// </summary>
+        /// <param name="uncommittedEvent">The <see cref="UncommittedEvent" />.</param>
+        /// <param name="cancellationToken">Token that can be used to cancel this operation.</param>
+        /// <returns>A <see cref="Task"/> that, when resolved, returns the <see cref="CommittedEvent" />.</returns>
+        Task<CommitEventsResult> Commit(UncommittedEvent uncommittedEvent, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Commits <see cref="UncommittedEvents" />.
+        /// </summary>
+        /// <param name="uncommittedEvents">The <see cref="UncommittedEvents" />.</param>
+        /// <param name="cancellationToken">Token that can be used to cancel this operation.</param>
+        /// <returns>A <see cref="Task"/> that, when resolved, returns the <see cref="CommittedEvents" />.</returns>
+        /// <remarks>
+        /// Cancelling this operation does not roll back the commit transaction if the events have already been written to the Event Store.
+        /// </remarks>
+        Task<CommitEventsResult> Commit(UncommittedEvents uncommittedEvents, CancellationToken cancellationToken = default);
     }
 }
