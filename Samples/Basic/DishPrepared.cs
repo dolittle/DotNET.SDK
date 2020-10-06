863466cddf7fb8d9256c889edf0d4a8716cef04b
Joel Hoisko <joel.hoisko@gmail.com>
2020-10-06 12:20:23 +0200

    Add urls to tutorials

--- Samples/Basic/DishPrepared.cs
+++ Samples/Basic/DishPrepared.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// Sample code for the tutorial at https://dolittle.io/tutorial/getting-started/csharp/
 
 using Dolittle.SDK.Events;
 
