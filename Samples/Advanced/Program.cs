c065e4e1a5d34efa110e12978b3808e1dd5ace58
Sindre Wilting <sindrewilting@gmail.com>
2020-10-09 13:11:50 +0200

    Fix advanced sample

--- Samples/Advanced/Program.cs
+++ Samples/Advanced/Program.cs
@@ -44,12 +44,9 @@ namespace Basic
                 .ForTenant("900893e7-c4cc-4873-8032-884e965e4b97")
                 .CommitForAggregate("b2410210-0ae3-4539-aada-fc962bdd1303")
                 .WithEventSource("8ac5b16a-0b88-4578-a005-e5247c611777")
-                .ExpectVersion(0)
+                .ExpectVersion(1)
                 .Commit(eventsBuilder =>
                     eventsBuilder.CreateEvent(myEvent));
-            Console.WriteLine(commit.Result.Events);
-            Console.WriteLine(aggregateCommit.Result.Events);
-            Console.WriteLine(aggregateCommit.Result.Failure);
 
             var fetch = client.EventStore
                 .ForTenant("900893e7-c4cc-4873-8032-884e965e4b97")
