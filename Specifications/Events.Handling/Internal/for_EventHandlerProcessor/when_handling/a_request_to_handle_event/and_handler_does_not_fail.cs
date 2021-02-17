a666854da243b0c17f232d37c5600d5b262ccbea
woksin <sindrewilting@gmail.com>
2021-02-17 11:02:55 +0100

    Fix specs

--- Specifications/Events.Handling/Internal/for_EventHandlerProcessor/when_handling/a_request_to_handle_event/and_handler_does_not_fail.cs
+++ Specifications/Events.Handling/Internal/for_EventHandlerProcessor/when_handling/a_request_to_handle_event/and_handler_does_not_fail.cs
@@ -34,6 +34,7 @@ namespace Dolittle.SDK.Events.Handling.Internal.for_EventHandlerProcessor.when_h
                 .Returns(stream_event);
             event_context = new EventContext(
                 committed_event.EventLogSequenceNumber,
+                committed_event.Type.To<EventType, EventTypeId>(),
                 committed_event.EventSourceId.To<EventSourceId>(),
                 committed_event.Occurred.ToDateTimeOffset(),
                 execution_context,
