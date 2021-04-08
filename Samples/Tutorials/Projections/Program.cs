dad00570c7ffbb22431cd7b88491c2f25beff63e
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-04-08 22:11:14 +0200

    Add some code to sample

--- Samples/Tutorials/Projections/Program.cs
+++ Samples/Tutorials/Projections/Program.cs
@@ -1,12 +1,14 @@
-using System;
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // Sample code for the tutorial at https://dolittle.io/tutorials/getting-started/projections/
 
+using System;
 using System.Threading.Tasks;
 using Dolittle.SDK;
+using Dolittle.SDK.Events;
 using Dolittle.SDK.Projections;
 using Dolittle.SDK.Tenancy;
+using Newtonsoft.Json.Linq;
 
 namespace Kitchen
 {
@@ -37,6 +39,22 @@ namespace Kitchen
                         });
                     projections.RegisterProjection<Menu>();
                 })
+                // .WithProjections(projections =>
+                // {
+                //     projections.CreateProjection("4a4c5b13-d4dd-4665-a9df-27b8e9b2054c")
+                //         .ForReadModel<Chef>()
+                //         .On<DishPrepared>(_ => _.KeyFromProperty(_ => _.Chef), (chef, @event, ctx) =>
+                //         {
+                //             chef.Name = @event.Chef;
+                //             chef.Dishes.Add(@event.Dish);
+                //             return chef;
+                //         })
+                //         .On<ChefFired>(_ => _.KeyFromProperty(_ => _.Chef), (chef, @event, ctx) =>
+                //         {
+                //             return ProjectionResult<Chef>.Delete;
+                //         });
+                //     projections.RegisterProjection<Menu>();
+                // })
                 .Build();
 
             var preparedTaco = new DishPrepared("Bean Blaster Taco", "Mr. Taco");
@@ -44,46 +62,52 @@ namespace Kitchen
             var chiliCannon = new DishPrepared("Chili Cannon Wrap", "Ms. TexMex");
             // var mrTacoFired = new ChefFired("Mr. Taco");
 
-            await client.EventStore
-                .ForTenant(TenantId.Development)
-                .Commit(eventsBuilder =>
-                {
-                    eventsBuilder
-                        .CreateEvent(preparedTaco)
-                        .FromEventSource("bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9");
-                    eventsBuilder
-                        .CreateEvent(avocadoArtillery)
-                        .FromEventSource("bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9");
-                    eventsBuilder
-                        .CreateEvent(chiliCannon)
-                        .FromEventSource("bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9");
-                    // eventsBuilder
-                    //     .CreateEvent(mrTacoFired)
-                    //     .FromEventSource("bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9");
-                }).ConfigureAwait(false);
+            // await client.EventStore
+            //     .ForTenant(TenantId.Development)
+            //     .Commit(eventsBuilder =>
+            //     {
+            //         eventsBuilder
+            //             .CreateEvent(preparedTaco)
+            //             .FromEventSource("bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9");
+            //         eventsBuilder
+            //             .CreateEvent(avocadoArtillery)
+            //             .FromEventSource("bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9");
+            //         eventsBuilder
+            //             .CreateEvent(chiliCannon)
+            //             .FromEventSource("bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9");
+            //         // eventsBuilder
+            //         //     .CreateEvent(mrTacoFired)
+            //         //     .FromEventSource("bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9");
+            //     }).ConfigureAwait(false);
 
-            await Task.WhenAny(PrintProjectionsAfterOneSecond(client), client.Start()).ConfigureAwait(false);
-        }
+            var started = client.Start();
 
-        static async Task PrintProjectionsAfterOneSecond(Client client)
-        {
-            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
-            var menu = await client.Projections
-                .ForTenant(TenantId.Development)
-                .Get<Menu>("bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9")
-                .ConfigureAwait(false);
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(true);
 
-            System.Console.WriteLine($"Menu consists of: {string.Join(", ", menu.State.Dishes)}");
+            Console.WriteLine("Getting menu");
+            var menu = await client.Projections.ForTenant(TenantId.Development).Get<Menu>("bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9").ConfigureAwait(false);
+            Console.WriteLine($"Menu consists of: {string.Join(", ", menu.State.Dishes)}");
 
-            var allChefs = await client.Projections
-                .ForTenant(TenantId.Development)
-                .GetAll<Chef>()
-                .ConfigureAwait(false);
-
-            foreach (var chef in allChefs)
-            {
-                System.Console.WriteLine($"Chef name: {chef.State.Name} and prepared dishes: {string.Join(",", chef.State.Dishes)}");
+            Console.WriteLine("Getting menu but specify projection and convert to chef");
+            try {
+                var specificMenu = await client.Projections.ForTenant(TenantId.Development).Get<Chef>("bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9", "0405b93f-1461-472c-bdc2-f89e0afd4dfe", ScopeId.Default).ConfigureAwait(false);
+                Console.WriteLine($"Specific menu is: {specificMenu.State.Name}");
+            } catch {
+                Console.WriteLine("Getting menu as chef failed as it should");
             }
+
+            Console.WriteLine("Getting untyped menu");
+            var untypedMenu = await client.Projections.ForTenant(TenantId.Development).Get("bfe6f6e4-ada2-4344-8a3b-65a3e1fe16e9", "0405b93f-1461-472c-bdc2-f89e0afd4dfe").ConfigureAwait(false);
+            Console.WriteLine($"Menu is ({untypedMenu.State.GetType()}) {untypedMenu.State}");
+
+            Console.WriteLine("Getting all chefs");
+            foreach (var (key, state) in await client.Projections.ForTenant(TenantId.Development).GetAll<Chef>().ConfigureAwait(false))
+            {
+                var chef = state.State;
+                Console.WriteLine($"Chef name: {chef.Name} and prepared dishes: {string.Join(",", chef.Dishes)}");
+            }
+
+            await started.ConfigureAwait(false);
         }
     }
 }
