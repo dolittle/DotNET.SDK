9e832f8bb20a18b01362ca0d70fd7807c04f0d5b
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-06-30 09:10:08 +0200

    Move EmbeddingResultType

--- Source/Embeddings/EmbeddingResultType.cs
+++ Source/Embeddings/EmbeddingResultType.cs
@@ -1,10 +1,10 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Dolittle.SDK.Embeddings.Internal
+namespace Dolittle.SDK.Embeddings
 {
     /// <summary>
-    /// Defines the different result types of <see cref="IEmbedding{TReadModel}.On(TReadModel, object, Events.EventType, EmbeddingProjectContext, System.Threading.CancellationToken)" />.
+    /// Defines the different result types of an embedding's on-methods.
     /// </summary>
     public enum EmbeddingResultType
     {
