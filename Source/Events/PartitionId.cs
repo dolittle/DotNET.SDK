70e55d5a1816895c70ddf34e92c7a91701533a0e
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-10-04 16:03:34 +0200

    Change Concept types

--- Source/Events/PartitionId.cs
+++ Source/Events/PartitionId.cs
@@ -9,25 +9,25 @@ namespace Dolittle.SDK.Events
     /// <summary>
     /// Represents the concept of a unique identifier for a partition.
     /// </summary>
-    public class PartitionId : ConceptAs<Guid>
+    public class PartitionId : ConceptAs<string>
     {
         /// <summary>
         /// The <see cref="PartitionId"/> that refers to no partition.
         /// </summary>
-        public static readonly PartitionId Unspecified = Guid.Empty;
+        public static readonly PartitionId Unspecified = string.Empty;
 
         /// <summary>
         /// Implicitly converts from a <see cref="Guid"/> to an <see cref="PartitionId"/>.
         /// </summary>
         /// <param name="partitionId">The <see cref="Guid"/> representation.</param>
         /// <returns>The converted <see cref="PartitionId"/>.</returns>
-        public static implicit operator PartitionId(Guid partitionId) => new PartitionId { Value = partitionId };
+        public static implicit operator PartitionId(Guid partitionId) => new PartitionId { Value = partitionId.ToString() };
 
         /// <summary>
         /// Implicitly converts from a <see cref="string"/> to an <see cref="PartitionId"/>.
         /// </summary>
         /// <param name="partitionId">The <see cref="string"/> representation.</param>
         /// <returns>The converted <see cref="PartitionId"/>.</returns>
-        public static implicit operator PartitionId(string partitionId) => new PartitionId { Value = Guid.Parse(partitionId) };
+        public static implicit operator PartitionId(string partitionId) => new PartitionId { Value = partitionId };
     }
 }
