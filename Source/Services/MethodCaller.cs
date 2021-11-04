20affc537cd0fefea99664a0c944b0c69e41d49b
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-11-04 13:40:24 +0100

    Add specialized exception for when Runtime is not available.

--- Source/Services/MethodCaller.cs
+++ Source/Services/MethodCaller.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Google.Protobuf;
@@ -41,15 +42,36 @@ namespace Dolittle.SDK.Services
             where TClientMessage : IMessage
             where TServerMessage : IMessage
         {
-            return method.Call(CreateChannel(), CreateCallOptions(token));
+            try
+            {
+                var originalStream = method.Call(CreateChannel(), CreateCallOptions(token));
+                return new AsyncDuplexStreamingCall<TClientMessage, TServerMessage>(
+                    new CatchingClientStreamWriter<TClientMessage>(_host, _port, originalStream.RequestStream),
+                    new CatchingAsyncStreamReader<TServerMessage>(_host, _port, originalStream.ResponseStream),
+                    originalStream.ResponseHeadersAsync,
+                    originalStream.GetStatus,
+                    originalStream.GetTrailers,
+                    originalStream.Dispose);
+            }
+            catch (Exception)
+            {
+                throw new CouldNotConnectToRuntime(_host, _port);
+            }
         }
 
         /// <inheritdoc/>
-        public Task<TServerMessage> Call<TClientMessage, TServerMessage>(ICanCallAUnaryMethod<TClientMessage, TServerMessage> method, TClientMessage request, CancellationToken token)
+        public async Task<TServerMessage> Call<TClientMessage, TServerMessage>(ICanCallAUnaryMethod<TClientMessage, TServerMessage> method, TClientMessage request, CancellationToken token)
             where TClientMessage : IMessage
             where TServerMessage : IMessage
         {
-            return method.Call(request, CreateChannel(), CreateCallOptions(token)).ResponseAsync;
+            try
+            {
+                return await method.Call(request, CreateChannel(), CreateCallOptions(token)).ResponseAsync.ConfigureAwait(false);
+            }
+            catch (RpcException exception) when (exception.StatusCode == StatusCode.Unavailable)
+            {
+                throw new CouldNotConnectToRuntime(_host, _port);
+            }
         }
 
         Channel CreateChannel() => new Channel(_host, _port, _channelCredentials, _channelOptions);
