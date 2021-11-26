97787af5aaa04ff9ea75841757db308df7d119c8
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-11-26 15:44:02 +0100

    Cleaning

--- Source/SDK/ServiceCollectionExtensions.cs
+++ Source/SDK/ServiceCollectionExtensions.cs
@@ -2,20 +2,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using Dolittle.SDK.DependencyInversion;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Dolittle.SDK
 {
-    // public static class ApplicationBuilderExtensions
-    // {
-    //     public static IApplicationBuilder UseDolittle(this IApplicationBuilder app)
-    //     {
-    //         
-    //     }
-    // }
-    
     /// <summary>
     /// Extension methods for <see cref="IServiceCollection"/> for adding setting up the <see cref="IDolittleClient"/>.
     /// </summary>
