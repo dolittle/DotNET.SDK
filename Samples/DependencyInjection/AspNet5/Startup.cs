262e63e95e6a7429318b76f94d0ff7debf7d32b5
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-11-28 07:28:38 +0100

    Update sample

--- Samples/DependencyInjection/AspNet5/Startup.cs
+++ Samples/DependencyInjection/AspNet5/Startup.cs
@@ -1,26 +1,13 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Concurrent;
-using System.Globalization;
-using System.Threading.Tasks;
 using Dolittle.SDK;
-using Dolittle.SDK.DependencyInversion;
+using Dolittle.SDK.Extensions.AspNet;
 using Dolittle.SDK.Samples.DependencyInjection.Shared;
-using Dolittle.SDK.Tenancy;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
-using Swashbuckle.SwaggerUi;
 
 public class Startup
 {
@@ -50,35 +37,20 @@ public class Startup
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
-        app.UseSwagger();
-        app.UseSwaggerUI(options =>
-        {
-            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-            options.RoutePrefix = string.Empty;
-        });
-
-        app.Use(async (context, next) =>
-        {
-            var dolittleClient = context.RequestServices.GetRequiredService<IDolittleClient>();
-            var tenantServices = dolittleClient.Services.ForTenant(TenantId.Development);
-            var scopeFactory = tenantServices.GetRequiredService<IServiceScopeFactory>();
-            using var scope = scopeFactory.CreateScope();
-            context.RequestServices = scope.ServiceProvider;
-            await next();
-        });
-        app.UseHttpsRedirection();
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
-        // app.UseMiddleware<>()
+        app.UseDeveloperExceptionPage()
+            .UseSwagger()
+            .UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            })
+            .UseDolittle()
+            .UseHttpsRedirection()
+            .UseRouting()
+            .UseAuthorization()
+            .UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
     }
 }
