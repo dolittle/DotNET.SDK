80493ae2c4f249cd94a44ce3c50919edfc9e84af
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-12-10 13:30:22 +0100

    Change ITenants to take in execution context + update to C#10

--- Source/Tenancy.Client/FailedToGetAllTenants.cs
+++ Source/Tenancy.Client/FailedToGetAllTenants.cs
@@ -2,23 +2,20 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using Dolittle.Protobuf.Contracts;
-using Dolittle.SDK.Protobuf;
 
-namespace Dolittle.SDK.Tenancy.Client
+namespace Dolittle.SDK.Tenancy.Client;
+
+/// <summary>
+/// Exception that gets thrown when getting all tenants failed.
+/// </summary>
+public class FailedToGetAllTenants : Exception
 {
     /// <summary>
-    /// Exception that gets thrown when getting all tenants failed.
+    /// Initializes a new instance of the <see cref="FailedToGetAllTenants"/> class.
     /// </summary>
-    public class FailedToGetAllTenants : Exception
+    /// <param name="failure">The reason for why it failed.</param>
+    public FailedToGetAllTenants(string failure)
+        : base($"Failed to get all tenants because {failure}")
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FailedToGetAllTenants"/> class.
-        /// </summary>
-        /// <param name="failure">The reason for why it failed.</param>
-        public FailedToGetAllTenants(string failure)
-            : base($"Failed to get all tenants because {failure}")
-        {
-        }
     }
 }
--- Source/Tenancy.Client/Internal/TenantsClient.cs
+++ Source/Tenancy.Client/Internal/TenantsClient.cs
@@ -11,58 +11,53 @@ using Dolittle.SDK.Protobuf;
 using Dolittle.SDK.Services;
 using Dolittle.Services.Contracts;
 using Microsoft.Extensions.Logging;
-using Contracts = Dolittle.Runtime.Tenancy.Contracts;
 using ExecutionContext = Dolittle.SDK.Execution.ExecutionContext;
 
-namespace Dolittle.SDK.Tenancy.Client.Internal
+namespace Dolittle.SDK.Tenancy.Client.Internal;
+
+/// <summary>
+/// Represents a client for <see cref="Runtime.Tenancy.Contracts.Tenants"/> and an implementation of <see cref="ITenants"/>.
+/// </summary>
+public class TenantsClient : ITenants
 {
+    static readonly TenantsGetAllMethod _method = new();
+    readonly IPerformMethodCalls _caller;
+    readonly ILogger _logger;
+
     /// <summary>
-    /// Represents a client for <see cref="Contracts.Tenants"/> and an implementation of <see cref="ITenants"/>.
+    /// Initializes a new instance of the <see cref="TenantsClient"/> class.
     /// </summary>
-    public class TenantsClient : ITenants
+    /// <param name="caller">The method caller to use to perform calls to the Runtime.</param>
+    /// <param name="logger">The <see cref="ILogger"/> to use.</param>
+    public TenantsClient(IPerformMethodCalls caller, ILogger logger)
     {
-        static readonly TenantsGetAllMethod _method = new TenantsGetAllMethod();
-        readonly IPerformMethodCalls _caller;
-        readonly ExecutionContext _executionContext;
-        readonly ILogger _logger;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TenantsClient"/> class.
-        /// </summary>
-        /// <param name="caller">The method caller to use to perform calls to the Runtime.</param>
-        /// <param name="executionContext">The <see cref="ExecutionContext"/>.</param>
-        /// <param name="logger">The <see cref="ILogger"/> to use.</param>
-        public TenantsClient(IPerformMethodCalls caller, ExecutionContext executionContext, ILogger logger)
-        {
-            _caller = caller;
-            _executionContext = executionContext;
-            _logger = logger;
-        }
-
-        /// <inheritdoc />
-        public async Task<IEnumerable<Tenant>> GetAll(CancellationToken cancellationToken = default)
-        {
-            _logger.LogDebug("Getting all Tenants");
-            try
-            {
-                var request = new GetAllRequest{ CallContext = new CallRequestContext { ExecutionContext = _executionContext.ToProtobuf() } };
-                var response = await _caller.Call(_method, request, cancellationToken).ConfigureAwait(false);
-                if (response.Failure == null)
-                {
-                    return response.Tenants.Select(CreateTenant);
-                }
-
-                _logger.LogWarning("An error occurred while getting all Tenants because {Reason}. Failure Id '{FailureId}'", response.Failure.Reason, response.Failure.Id.ToGuid().ToString());
-                throw new FailedToGetAllTenants(response.Failure.Reason);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogWarning(ex, "An error occurred while getting all Tenants");
-                throw;
-            }
-        }
-
-        static Tenant CreateTenant(Contracts.Tenant tenant)
-            => new Tenant(tenant.Id.ToGuid());
+        _caller = caller;
+        _logger = logger;
     }
+
+    /// <inheritdoc />
+    public async Task<IEnumerable<Tenant>> GetAll(ExecutionContext executionContext, CancellationToken cancellationToken = default)
+    {
+        Log.GettingAllTenants(_logger);
+        try
+        {
+            var request = new GetAllRequest { CallContext = new CallRequestContext { ExecutionContext = executionContext.ToProtobuf() } };
+            var response = await _caller.Call(_method, request, cancellationToken).ConfigureAwait(false);
+            if (response.Failure == null)
+            {
+                return response.Tenants.Select(CreateTenant);
+            }
+
+            Log.FailedGettingAllTenants(_logger, response.Failure.Reason, response.Failure.Id.ToGuid());
+            throw new FailedToGetAllTenants(response.Failure.Reason);
+        }
+        catch (Exception ex)
+        {
+            Log.ErrorGettingTenants(_logger, ex);
+            throw;
+        }
+    }
+
+    static Tenant CreateTenant(Runtime.Tenancy.Contracts.Tenant tenant)
+        => new(tenant.Id.ToGuid());
 }
