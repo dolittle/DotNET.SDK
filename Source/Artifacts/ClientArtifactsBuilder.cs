3c89d8ba308e800bec7fe2ee09e4c13c950b4b72
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2021-12-17 13:07:32 +0100

    Implement AddAllFrom on IArtifactsBuilder instead

--- Source/Artifacts/ClientArtifactsBuilder.cs
+++ Source/Artifacts/ClientArtifactsBuilder.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Reflection;
 using Dolittle.SDK.Common;
 
 namespace Dolittle.SDK.Artifacts;
@@ -13,10 +14,21 @@ namespace Dolittle.SDK.Artifacts;
 /// <typeparam name="TId">The <see cref="Type" /> of the <see cref="ArtifactId" />.</typeparam>
 /// <typeparam name="TUniqueBindings">The <see cref="Type"/> of the <see cref="IUniqueBindings{TIdentifier,TValue}"/> to be built</typeparam>
 /// <typeparam name="TArtifactAttribute">The <see cref="Type"/> of the <see cref="Attribute"/> used to mark the artifact.</typeparam>
-public abstract class ClientArtifactsBuilder<TArtifact, TId, TUniqueBindings, TArtifactAttribute> : ClientUniqueDecoratedBindingsBuilder<TArtifact, TUniqueBindings, TArtifactAttribute>, IArtifactsBuilder<TArtifact, TId, TUniqueBindings>
+public abstract class ClientArtifactsBuilder<TArtifact, TId, TUniqueBindings, TArtifactAttribute> : ClientUniqueDecoratedBindingsBuilder<TArtifact, Type, TUniqueBindings, TArtifactAttribute>, IArtifactsBuilder<TArtifact, TId, TUniqueBindings>
     where TArtifact : Artifact<TId>, IEquatable<TArtifact>
     where TId : ArtifactId, IEquatable<TId>
     where TUniqueBindings : IUniqueBindings<TArtifact, Type>
     where TArtifactAttribute : Attribute
 {
+    /// <inheritdoc />
+    public void AddAllFrom(Assembly assembly)
+    {
+        foreach (var type in assembly.ExportedTypes)
+        {
+            if (TryGetDecorator(type, out _))
+            {
+                Add(type);
+            }
+        }
+    }
 }
--- Source/Artifacts/IArtifactsBuilder.cs
+++ Source/Artifacts/IArtifactsBuilder.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Reflection;
 using Dolittle.SDK.Common;
 
 namespace Dolittle.SDK.Artifacts;
@@ -12,9 +13,14 @@ namespace Dolittle.SDK.Artifacts;
 /// <typeparam name="TArtifact">The <see cref="Type" /> of the <see cref="Artifact{TId}" />.</typeparam>
 /// <typeparam name="TId">The <see cref="Type" /> of the <see cref="ArtifactId" />.</typeparam>
 /// <typeparam name="TUniqueBindings">The <see cref="Type"/> of the <see cref="IUniqueBindings{TIdentifier,TValue}"/> to be built</typeparam>
-public interface IArtifactsBuilder<in TArtifact, TId, out TUniqueBindings> : ICanBuildUniqueDecoratedBindings<TArtifact, TUniqueBindings>
+public interface IArtifactsBuilder<in TArtifact, TId, out TUniqueBindings> : ICanBuildUniqueDecoratedBindings<TArtifact, Type, TUniqueBindings>
     where TArtifact : Artifact<TId>, IEquatable<TArtifact>
     where TId : ArtifactId, IEquatable<TId>
     where TUniqueBindings : IUniqueBindings<TArtifact, Type>
 {
+    /// <summary>
+    /// Adds all bindings discovered in the <see cref="Assembly"/>.
+    /// </summary>
+    /// <param name="assembly"><see cref="Assembly"/> to add all bindings from.</param>
+    void AddAllFrom(Assembly assembly);
 }
