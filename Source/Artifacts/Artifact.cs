e8d9bbe2a66e1f91593ab56048ea4ffb3d78a5a7
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2022-01-12 10:44:03 +0100

    Make Artifact into an IIdentifier

--- Source/Artifacts/Artifact.cs
+++ Source/Artifacts/Artifact.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using Dolittle.SDK.Common.Model;
+using Dolittle.SDK.Concepts;
 
 namespace Dolittle.SDK.Artifacts;
 
@@ -11,7 +13,7 @@ namespace Dolittle.SDK.Artifacts;
 /// <typeparam name="TId">The <see cref="Type" /> of the <see cref="ArtifactId" />.</typeparam>
 /// <param name="Id"><typeparamref name="TId">Id</typeparamref> of the <see cref="Artifact{TId}"/>.</param>
 /// <param name="Generation"><see cref="Generation">Generation</see> of the <see cref="Artifact{TId}"/>.</param>
-public abstract record Artifact<TId>(TId Id, Generation Generation)
+public abstract record Artifact<TId>(TId Id, Generation Generation) : IIdentifier<TId>
     where TId : ArtifactId
 {
     /// <summary>
@@ -22,4 +24,21 @@ public abstract record Artifact<TId>(TId Id, Generation Generation)
         : this(id, Generation.First)
     {
     }
+
+    /// <inheritdoc />
+    Guid IIdentifier.Id => Id.Value;
+
+    /// <inheritdoc />
+    public bool CanCoexistWith(IIdentifier<ConceptAs<Guid>> identifier)
+    {
+        if (identifier.GetType() != GetType())
+        {
+            return false;
+        }
+        var artifact = identifier as Artifact<TId>;
+        return Id.Equals(artifact?.Id) && !Generation.Equals(artifact?.Generation);
+    }
+
+    /// <inheritdoc />
+    public bool CanCoexistWith(IIdentifier identifier) => identifier is IIdentifier<TId> typedIdentifier && CanCoexistWith((IIdentifier<ConceptAs<Guid>>)typedIdentifier);
 }
