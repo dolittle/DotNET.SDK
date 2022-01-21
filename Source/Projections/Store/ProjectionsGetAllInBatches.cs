3cd308091d6f11712375df1fd14615c49f58ea80
woksin <sindrewilting@gmail.com>
2022-01-21 12:13:04 +0100

    Update to new contracts and add ProjectionsGetAllInBatches method

---
+++ Source/Projections/Store/ProjectionsGetAllInBatches.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Dolittle.Runtime.Projections.Contracts;
+using Dolittle.SDK.Services;
+using Grpc.Core;
+
+namespace Dolittle.SDK.Projections.Store;
+
+/// <summary>
+/// Represents a wrapper for gRPC Projections.GetAllInBatches
+/// </summary>
+public class ProjectionsGetAllInBatches : ICanCallAServerStreamingMethod<GetAllRequest, GetAllResponse>
+{
+    /// <inheritdoc/>
+    public AsyncServerStreamingCall<GetAllResponse> Call(GetAllRequest message, Channel channel, CallOptions callOptions)
+    {
+        var client = new Runtime.Projections.Contracts.Projections.ProjectionsClient(channel);
+        return client.GetAllInBatches(message, callOptions);
+    }
+}
