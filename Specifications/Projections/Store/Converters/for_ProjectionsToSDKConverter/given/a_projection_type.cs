0fcc3b4a297e779d94b20d789aedc8bfa1051859
woksin <sindrewilting@gmail.com>
2022-01-23 21:25:04 +0100

    Specs for converting a single projection

---
+++ Specifications/Projections/Store/Converters/for_ProjectionsToSDKConverter/given/a_projection_type.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Dolittle.SDK.Projections.Store.Converters.for_ProjectionsToSDKConverter.given;
+
+public record a_projection_type
+{
+    public string AValue { get; set; }
+}
---
+++ Specifications/Projections/Store/Converters/for_ProjectionsToSDKConverter/given/a_projection_type_with_additional_fields.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Dolittle.SDK.Projections.Store.Converters.for_ProjectionsToSDKConverter.given;
+
+public record a_projection_type_with_additional_fields
+{
+    public string AValue { get; set; }
+    public int AnotherValue { get; set; }
+}
