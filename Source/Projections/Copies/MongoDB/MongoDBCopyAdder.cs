98cd011df4ce511adb6f855784030e512e7c237f
woksin <sindrewilting@gmail.com>
2022-01-27 21:48:19 +0100

    Implement MongoDBCopyAdder

--- Source/Projections/Copies/MongoDB/MongoDBCopyAdder.cs
+++ Source/Projections/Copies/MongoDB/MongoDBCopyAdder.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Dolittle.SDK.Common;
 using Dolittle.SDK.Common.ClientSetup;
 
 namespace Dolittle.SDK.Projections.Copies.MongoDB;
@@ -10,8 +11,57 @@ namespace Dolittle.SDK.Projections.Copies.MongoDB;
 /// </summary>
 public class MongoDBCopyAdder : ICanAugmentProjectionCopy
 {
+    readonly IResolveConversions _conversionsResolver;
+    readonly IValidateMongoDBCollectionName _collectionNameValidator;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MongoDBCopyAdder"/> class.
+    /// </summary>
+    /// <param name="conversionsResolver">The <see cref="IResolveConversions" />.</param>
+    /// <param name="collectionNameValidator">The <see cref="IValidateMongoDBCollectionName"/>.</param>
+    public MongoDBCopyAdder(IResolveConversions conversionsResolver, IValidateMongoDBCollectionName collectionNameValidator)
+    {
+        _conversionsResolver = conversionsResolver;
+        _collectionNameValidator = collectionNameValidator;
+    }
+
+    /// <inheritdoc />
+    public bool CanAugment<TProjection>()
+        where TProjection : class, new()
+        => typeof(TProjection).TryGetDecorator<CopyProjectionToMongoDBAttribute>(out _);
+
     /// <inheritdoc />
     public bool TryAugment<TProjection>(IClientBuildResults buildResults, ProjectionCopies projectionCopies, out ProjectionCopies augmentedProjectionCopies)
         where TProjection : class, new()
-        => throw new System.NotImplementedException();
+    {
+        var succeeded = true;
+        if (!ProjectionMongoDBCopyCollectionName.TryGetFrom<TProjection>(out var collectionName))
+        {
+            buildResults.AddFailure($"Could not get projection MongoDB Copy collection name from projection read model type {nameof(TProjection)}");
+            succeeded = false;
+        }
+        if (collectionName != default && !_collectionNameValidator.Validate(buildResults, collectionName))
+        {
+            buildResults.AddFailure($"MongoDB Copy collection name {collectionName} from projection read model type {nameof(TProjection)} is not valid");
+            succeeded = false;
+        }
+        if (!_conversionsResolver.TryResolve<TProjection>(buildResults, out var conversions))
+        {
+            buildResults.AddFailure($"Could not get projection MongoDB Copy conversions from projection read model type {nameof(TProjection)}");
+            succeeded = false;
+        }
+
+        if (!succeeded)
+        {
+            augmentedProjectionCopies = default;
+            return false;
+        }
+
+        augmentedProjectionCopies = projectionCopies with
+        {
+            MongoDB = new ProjectionCopyToMongoDB(collectionName, conversions)
+        };
+
+        return true;
+    }
 }
--- Source/Projections/Copies/MongoDB/ProjectionCopyToMongoDB.cs
+++ Source/Projections/Copies/MongoDB/ProjectionCopyToMongoDB.cs
@@ -2,12 +2,17 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
+using MongoDB.Bson;
 using PbProjectionCopyToMongoDB = Dolittle.Runtime.Events.Processing.Contracts.ProjectionCopyToMongoDB;
 
 namespace Dolittle.SDK.Projections.Copies.MongoDB;
 
-public record ProjectionCopyToMongoDB(ProjectionMongoDBCopyCollectionName CollectionName, IDictionary<string, PbProjectionCopyToMongoDB.Types.BSONType> Conversions)
+public record ProjectionCopyToMongoDB(ProjectionMongoDBCopyCollectionName CollectionName, IDictionary<string, BsonType> Conversions)
 {
+    /// <summary>
+    /// Creates a Protobuf representation of this <see cref="ProjectionCopyToMongoDB"/>.
+    /// </summary>
+    /// <returns><see cref="PbProjectionCopyToMongoDB"/>.</returns>
     public PbProjectionCopyToMongoDB ToProtobuf()
     {
         var result = new PbProjectionCopyToMongoDB
@@ -17,9 +22,17 @@ public record ProjectionCopyToMongoDB(ProjectionMongoDBCopyCollectionName Collec
         
         foreach (var (fieldName, type) in Conversions)
         {
-            result.Conversions.Add(fieldName, type);
+            result.Conversions.Add(fieldName, ToProtobuf(type));
         }
         return result;
     }
-}
 
+    static PbProjectionCopyToMongoDB.Types.BSONType ToProtobuf(BsonType type)
+        => type switch
+        {
+            BsonType.Binary => PbProjectionCopyToMongoDB.Types.BSONType.Binary,
+            BsonType.DateTime => PbProjectionCopyToMongoDB.Types.BSONType.Date,
+            BsonType.Timestamp => PbProjectionCopyToMongoDB.Types.BSONType.Timestamp,
+            _ => throw new UnsupportedBSONType(type)
+        };
+}
