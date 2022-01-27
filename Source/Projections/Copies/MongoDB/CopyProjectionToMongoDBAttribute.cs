0624ee6866e8059f120fe7d207519929606e7fad
woksin <sindrewilting@gmail.com>
2022-01-27 12:52:42 +0100

    Introduce the CopyProjectionToMongoDBAttribute attribute to decorate on projection read model classes signifying that they should be stored in a MongoDB copy store

---
+++ Source/Projections/Copies/MongoDB/CopyProjectionToMongoDBAttribute.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Dolittle.SDK.Common.Model;
+using Dolittle.SDK.Events;
+
+namespace Dolittle.SDK.Projections.Copies.MongoDB;
+
+/// <summary>
+/// Decorates a projection class to indicate that the projection read models should be copied to a MongoDB store.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class CopyProjectionToMongoDBAttribute : Attribute
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CopyProjectionToMongoDBAttribute"/> class.
+    /// </summary>
+    /// <param name="collectionName">The unique identifier of the event handler.</param>
+    /// <param name="inScope">The scope that the event handler handles events in.</param>
+    public CopyProjectionToMongoDBAttribute(string collectionName = "")
+    {
+        CollectionName = collectionName;
+    }
+
+    /// <summary>
+    /// Gets the unique identifier for this projection.
+    /// </summary>
+    public ProjectionMongoDBCopyCollectionName CollectionName { get; }
+}
