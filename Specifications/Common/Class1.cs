06b9c9169ea2a364f21b47d32efd839db3915a87
woksin <sindrewilting@gmail.com>
2022-02-09 09:12:37 +0100

    Missing headers

--- Specifications/Common/Class1.cs
+++ Specifications/Common/Class1.cs
@@ -1,4 +1,7 @@
-﻿namespace Common;
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Common;
 
 public class Class1
 {
