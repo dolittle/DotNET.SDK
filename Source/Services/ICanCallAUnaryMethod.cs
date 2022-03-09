3c2b3d3503416cb51a3a9c1dd7e37ef74c82e986
Sindre Alstad Wilting <woksin@users.noreply.github.com>
2022-03-09 09:57:53 +0100

    Update Source/Services/ICanCallAUnaryMethod.cs
    Co-authored-by: Jakob Høgenes <1014990+jakhog@users.noreply.github.com>

--- Source/Services/ICanCallAUnaryMethod.cs
+++ Source/Services/ICanCallAUnaryMethod.cs
@@ -20,7 +20,7 @@ public interface ICanCallAUnaryMethod<TClientMessage, TServerMessage>
     /// Performs an async unary call on the server using the provided message, channel and call options.
     /// </summary>
     /// <param name="message">The message to send to the server.</param>
-    /// <param name="channel">The <see cref="Channel"/> to use to connect to the server.</param>
+    /// <param name="channel">The <see cref="ChannelBase"/> to use to connect to the server.</param>
     /// <param name="callOptions">The <see cref="CallOptions"/> to use for the call.</param>
     /// <returns>A <see cref="AsyncUnaryCall{TResult}"/> representing the result of the async unary call.</returns>
     AsyncUnaryCall<TServerMessage> Call(TClientMessage message, ChannelBase channel, CallOptions callOptions);
