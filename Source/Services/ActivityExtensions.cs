9a2b00e3684bcc0e9817a96abf1d6576b8a6c5eb
Magne Helleborg <magne.helleborg@gmail.com>
2022-06-16 15:11:33 +0200

    Populate Correlation & SpanId if there exists an active activity

---
+++ Source/Services/ActivityExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+using Dolittle.Execution.Contracts;
+using Dolittle.Protobuf.Contracts;
+using Google.Protobuf;
+
+namespace Dolittle.SDK.Services;
+
+static class ActivityExtensions
+{
+    public static void SetTraceContext(this Activity activity, ExecutionContext context)
+    {
+        context.CorrelationId = activity.TraceId.ToUuid();
+        context.SpanId = activity.SpanId.ToByteString();
+    }
+    
+    static Uuid ToUuid(this ActivityTraceId traceId)
+    {
+        var bytes = new byte[16];
+        traceId.CopyTo(bytes);
+        return new Uuid
+        {
+            Value = ByteString.CopyFrom(bytes)
+        };
+    }
+    
+    static ByteString ToByteString(this ActivitySpanId spanId)
+    {
+        var bytes = new byte[8];
+        spanId.CopyTo(bytes);
+        return ByteString.CopyFrom(bytes);
+    }
+}
