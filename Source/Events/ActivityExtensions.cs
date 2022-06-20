b45e5c4b69bbe0960ae0b3242f741847a612844f
Magne Helleborg <magne.helleborg@gmail.com>
2022-06-20 13:12:48 +0200

    Trace commits

--- Source/Events/ActivityExtensions.cs
+++ Source/Events/ActivityExtensions.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Diagnostics;
+using System.Linq;
+using Dolittle.SDK.Events.Store;
 
 namespace Dolittle.SDK.Events;
 
@@ -13,7 +15,7 @@ public static class ActivityExtensions
     const string EventType = "type.id";
     const string EventTypeAlias = "type.alias";
     const string EventSourceId = "eventsource.id";
-    
+
     /// <param name="activity">Activity to tag</param>
     /// <param name="eventType">EventType metadata</param>
     /// <returns></returns>
@@ -27,13 +29,26 @@ public static class ActivityExtensions
 
         return activity;
     }
-    
+
     /// <param name="activity">Activity to tag</param>
     /// <param name="eventSourceId">EventSourceId metadata</param>
     /// <returns></returns>
     public static Activity Tag(this Activity activity, EventSourceId eventSourceId)
     {
         activity.SetTag(EventSourceId, eventSourceId.Value);
+
+        return activity;
+    }
+
+    /// <param name="activity">Activity to tag</param>
+    /// <param name="uncommittedEvents">EventSourceId metadata</param>
+    /// <returns></returns>
+    public static Activity Tag(this Activity activity, UncommittedEvents uncommittedEvents)
+    {
+        foreach (var grouping in uncommittedEvents.GroupBy(@event => @event.EventType))
+        {
+            activity.SetTag($"{grouping.Key.Id}.count", grouping.Count());
+        }
 
         return activity;
     }
