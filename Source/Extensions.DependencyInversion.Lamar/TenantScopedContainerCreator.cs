42c69d43a44b0e0eea78b1680a665ecd7cea7999
woksin <sindrewilting@gmail.com>
2022-07-01 10:59:45 +0200

    Add DependencyInversion Lamar extension

---
+++ Source/Extensions.DependencyInversion.Lamar/TenantScopedContainerCreator.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Dolittle.SDK.DependencyInversion;
+using Lamar;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dolittle.SDK.Extensions.DependencyInversion.Lamar;
+
+/// <summary>
+/// Represents an implementation of <see cref="ICanCreateTenantScopedContainer"/> using Lamar <see cref="Container"/> as the child container.
+/// </summary>
+public class TenantScopedContainerCreator : ICanCreateTenantScopedContainer
+{
+    /// <inheritdoc />
+    public bool CanCreateFrom(IServiceProvider rootProvider)
+        => rootProvider is IContainer;
+
+    /// <inheritdoc />
+    public IServiceProvider Create(IServiceProvider rootProvider, IServiceCollection tenantScopedServices)
+    {
+        var rootContainer = (rootProvider as IContainer)!;
+        var tenantScopedContainer = rootContainer.GetNestedContainer() as Container;
+        tenantScopedContainer!.Configure(tenantScopedServices);
+        return tenantScopedContainer;
+    }
+}
