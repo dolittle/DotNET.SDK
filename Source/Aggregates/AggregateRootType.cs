96cf9de9bd5ed93b6144e7015571e30dd275a430
woksin <sindrewilting@gmail.com>
2022-08-26 11:11:43 +0200

    Addapt aggregates for changes

--- Source/Aggregates/AggregateRootType.cs
+++ Source/Aggregates/AggregateRootType.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Dolittle.SDK.ApplicationModel;
 using Dolittle.SDK.Artifacts;
 using Dolittle.SDK.Events;
 
@@ -15,68 +16,10 @@ public class AggregateRootType : Artifact<AggregateRootId>
     /// Initializes a new instance of the <see cref="AggregateRootType"/> class.
     /// </summary>
     /// <param name="id">The <see cref="AggregateRootId">unique identifier</see> of the <see cref="AggregateRootType"/>.</param>
-    public AggregateRootType(AggregateRootId id)
-        : this(id, Generation.First, null)
-    {
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="AggregateRootType"/> class.
-    /// </summary>
-    /// <param name="id">The <see cref="AggregateRootId">unique identifier</see> of the <see cref="AggregateRootType"/>.</param>
-    /// <param name="alias"><see cref="AggregateRootAlias">Alias</see> of the <see cref="AggregateRootType"/>.</param>
-    public AggregateRootType(AggregateRootId id, AggregateRootAlias alias)
-        : this(id, Generation.First, alias)
-    {
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="AggregateRootType"/> class.
-    /// </summary>
-    /// <param name="id">The <see cref="AggregateRootId">unique identifier</see> of the <see cref="AggregateRootType"/>.</param>
     /// <param name="generation"><see cref="Generation">Generation</see> of the <see cref="AggregateRootType"/>.</param>
-    public AggregateRootType(AggregateRootId id, Generation generation)
-        : this(id, generation, null)
+    /// <param name="alias"><see cref="IdentifierAlias">Alias</see> of the <see cref="AggregateRootType"/>.</param>
+    public AggregateRootType(AggregateRootId id, Generation? generation = null, IdentifierAlias? alias = null)
+        : base(id, generation, alias)
     {
-    }
-    
-    /// <summary>
-    /// Initializes a new instance of the <see cref="AggregateRootType"/> class.
-    /// </summary>
-    /// <param name="id">The <see cref="AggregateRootId">unique identifier</see> of the <see cref="AggregateRootType"/>.</param>
-    /// <param name="generation"><see cref="Generation">Generation</see> of the <see cref="AggregateRootType"/>.</param>
-    /// <param name="alias"><see cref="AggregateRootAlias">Alias</see> of the <see cref="AggregateRootType"/>.</param>
-    public AggregateRootType(AggregateRootId id, Generation generation, AggregateRootAlias alias)
-        : base(id, generation)
-    {
-        ThrowIfAggregateRootTypeIdIsNull(id);
-        ThrowIfGenerationIsNull(generation);
-        Alias = alias;
-    }
-
-    /// <summary>
-    /// Gets the alias for the Aggregate Root.
-    /// </summary>
-    public AggregateRootAlias? Alias { get; }
-
-    /// <summary>
-    /// Gets a value indicating whether the Aggregate Root has an alias or not.
-    /// </summary>
-    public bool HasAlias => !string.IsNullOrEmpty(Alias?.Value);
-
-    static void ThrowIfAggregateRootTypeIdIsNull(AggregateRootId id)
-    {
-        if (id == null)
-        {
-            throw new AggregateRootIdCannotBeNull();
-        }
-    }
-
-    static void ThrowIfGenerationIsNull(Generation generation)
-    {
-        if (generation == null)
-        {
-            throw new AggregateRootTypeGenerationCannotBeNull();
-        }
     }
 }
--- Source/Aggregates/Internal/Log.cs
+++ Source/Aggregates/Internal/Log.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using Dolittle.SDK.ApplicationModel;
 using Dolittle.SDK.Events;
 using Microsoft.Extensions.Logging;
 
@@ -16,11 +17,11 @@ static partial class Log
     internal static partial void Get(this ILogger logger, Type aggregateRoot, EventSourceId eventSource);
 
     [LoggerMessage(0, LogLevel.Debug, "Registering Aggregate Root {AggregateRoot} with Alias {Alias}")]
-    internal static partial void Registering(this ILogger logger, AggregateRootId aggregateRoot, AggregateRootAlias alias);
+    internal static partial void Registering(this ILogger logger, AggregateRootId aggregateRoot, IdentifierAlias alias);
 
     [LoggerMessage(0, LogLevel.Warning, "A failure occurred while registering Aggregate Root {AggregateRoot} with Alias {Alias} because {Reason}")]
-    internal static partial void FailedToRegister(this ILogger logger, AggregateRootId aggregateRoot, AggregateRootAlias alias, string reason);
+    internal static partial void FailedToRegister(this ILogger logger, AggregateRootId aggregateRoot, IdentifierAlias alias, string reason);
     
     [LoggerMessage(0, LogLevel.Warning,"An error occurred while registering Aggregate Root {AggregateRoot} with Alias {Alias}" )]
-    internal static partial void ErrorDuringRegister(this ILogger logger, AggregateRootId aggregateRoot, AggregateRootAlias alias, Exception exception);
+    internal static partial void ErrorDuringRegister(this ILogger logger, AggregateRootId aggregateRoot, IdentifierAlias alias, Exception exception);
 }
