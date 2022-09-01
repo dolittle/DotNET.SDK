07d7bc3fb40eced0c9e7ae96a32466d410c4bb8d
woksin <sindrewilting@gmail.com>
2022-09-01 11:19:24 +0200

    Add method to IFetchEventsForAggregate that filters on event types

--- Source/Events/Store/IFetchEventsForAggregate.cs
+++ Source/Events/Store/IFetchEventsForAggregate.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -21,5 +23,22 @@ public interface IFetchEventsForAggregate
     Task<CommittedAggregateEvents> FetchForAggregate(
         AggregateRootId aggregateRootId,
         EventSourceId eventSourceId,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Fetches the <see cref="CommittedAggregateEvents" /> for an aggregate root.
+    /// </summary>
+    /// <param name="aggregateRootId">The <see cref="AggregateRootId" /> of the aggregate root.</param>
+    /// <param name="eventSourceId">The <see cref="EventSourceId"/>.</param>
+    /// <param name="eventTypes">
+    /// The <see cref="IEnumerable{T}"/> of <see cref="EventType"/> of the events to fetch.
+    /// If none event types are given then it will get all the committed aggregate events for the aggregate root.
+    /// </param>
+    /// <param name="cancellationToken">Token that can be used to cancel this operation.</param>
+    /// <returns>A <see cref="Task"/> that, when resolved, returns the <see cref="CommittedAggregateEvents" />.</returns>
+    Task<CommittedAggregateEvents> FetchForAggregate(
+        AggregateRootId aggregateRootId,
+        EventSourceId eventSourceId,
+        IEnumerable<EventType> eventTypes,
         CancellationToken cancellationToken = default);
 }
