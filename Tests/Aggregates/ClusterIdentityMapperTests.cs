11f59a50557586371bfac96832cc2aa64357762e
Magne Helleborg <magne.helleborg@gmail.com>
2022-11-21 18:14:36 +0100

    Tests for mapping from / to cluster identity for aggregate

---
+++ Tests/Aggregates/ClusterIdentityMapperTests.cs
@@ -0,0 +1,27 @@
+﻿using Dolittle.SDK.Aggregates.Actors;
+using Dolittle.SDK.Events;
+using Dolittle.SDK.Tenancy;
+using FluentAssertions;
+
+namespace Aggregates;
+
+public class ClusterIdentityMapperTests
+{
+    [Theory]
+    [InlineData("1ea76ca4-a251-4e89-a409-bbc03170637a", "c77bca02-c95c-4171-bd14-86236fe32118")]
+    [InlineData("1ea76ca4-a251-4e89-a409-bbc03170637b", "foo")]
+    [InlineData("1ea76ca4-a251-4e89-a409-bbc03170637c", "foo/bar")]
+    [InlineData("1ea76ca4-a251-4e89-a409-bbc03170637d", "foo:bar")]
+    [InlineData("1ea76ca4-a251-4e89-a409-bbc03170637d", "foo:bar:baz")]
+    [InlineData("1ea76ca4-a251-4e89-a409-bbc03170637d", ":foo:bar:baz-waz")]
+    [InlineData("1ea76ca4-a251-4e89-a409-bbc03170637d", "123_foo:bar:baz-waz")]
+    public void CanMapIdentityCorrectly(TenantId tenantId, EventSourceId eventSourceId)
+    {
+        var clusterIdentity = ClusterIdentityMapper.GetClusterIdentity<TestAggregate>(tenantId, eventSourceId);
+
+        var (outputTenantId, outputEventSourceId) = ClusterIdentityMapper.GetTenantAndEventSourceId(clusterIdentity);
+
+        outputTenantId.Should().Be(tenantId);
+        outputEventSourceId.Should().Be(eventSourceId);
+    }
+}
---
+++ Tests/Aggregates/TestAggregate.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Dolittle.SDK.Aggregates;
+
+namespace Aggregates;
+
+[AggregateRoot("a1b2c3d4-e5f6-a7b8-c9d0-e1f2a3b4c5d6")]
+public class TestAggregate : AggregateRoot
+{
+}
