f0a0138740efff4690ff479d03e91fc91de01c33
woksin <sindrewilting@gmail.com>
2023-01-13 12:19:57 +0100

    Specs for ASP.NET sample

---
+++ Samples/ASP.NET.Specs/for_DishServer/when_two_dishes_have_been_prepared.cs
@@ -0,0 +1,50 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Customers;
+using Kitchen;
+using Machine.Specifications;
+
+namespace Specs.for_DishServer;
+
+class when_two_dishes_have_been_prepared : given.the_handler
+{
+    static DishPrepared first_dish, second_dish;
+    
+    Establish context = () =>
+    {
+        first_dish = @event with
+        {
+            Dish = "first dish",
+            Chef = "first chef"
+        };
+        second_dish = @event with
+        {
+            Dish = "second dish",
+            Chef = "second chef"
+        };
+
+        handler.Handle(first_dish, Specs.given.an_event_context_for<DishPrepared>(kitchen_name)).GetAwaiter().GetResult();
+    };
+    
+    Because of = async () => await handler.Handle(second_dish, Specs.given.an_event_context_for<DishPrepared>(kitchen_name));
+
+    It should_get_the_correct_customer = () => customers.TryGetAggregate(customer_name, out _).ShouldBeTrue(); 
+    
+    It the_customer_should_have_only_eaten_second_dish_after_last_operation = () => customers.AfterLastOperationOn(customer_name).ShouldHaveEvent<DishEaten>()
+        .CountOf(1)
+        .First().Where(_ => _.Dish.ShouldEqual(second_dish.Dish));
+
+    It the_customer_should_have_only_eaten_one_dish_after_last_operation = () => customers.AfterLastOperationOn(customer_name).ShouldHaveNumberOfEvents(1);
+
+    It the_customer_should_have_eaten_first_dish_first = () => customers.AssertThat(customer_name).ShouldHaveEvent<DishEaten>()
+        .AtBeginning()
+        .Where(_ => _.Dish.ShouldEqual(first_dish.Dish));
+    
+    It the_customer_should_have_eaten_second_dish_last = () => customers.AssertThat(customer_name).ShouldHaveEvent<DishEaten>()
+        .AtEnd()
+        .Where(_ => _.Dish.ShouldEqual(second_dish.Dish));
+
+    It the_customer_should_have_only_eaten_two_dishes = () => customers.AssertThat(customer_name).ShouldHaveNumberOfEvents(2);
+    
+}
---
+++ Samples/ASP.NET.Specs/given.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using Dolittle.SDK.Common.Model;
+using Dolittle.SDK.Events;
+using Dolittle.SDK.Execution;
+using Dolittle.SDK.Security;
+using Version = Dolittle.SDK.Microservices.Version;
+
+namespace Specs;
+
+static class given
+{
+    internal static EventContext an_event_context_for<TEvent>(EventSourceId eventSource, EventLogSequenceNumber sequenceNumber = null)
+    {
+        if (!typeof(TEvent).TryGetIdentifier(out EventType eventType))
+        {
+            throw new ArgumentException($"{typeof(TEvent)} is not decorated with {nameof(EventTypeAttribute)} attribute");
+        }
+        return new EventContext(
+            sequenceNumber ?? 0,
+            eventType,
+            eventSource,
+            DateTimeOffset.Now,
+            new ExecutionContext(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Version.NotSet,
+                "env",
+                Guid.NewGuid(),
+                Claims.Empty,
+                CultureInfo.InvariantCulture),
+            new ExecutionContext(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Version.NotSet,
+                "env",
+                Guid.NewGuid(),
+                Claims.Empty,
+                CultureInfo.InvariantCulture)
+        );
+    }
+}
