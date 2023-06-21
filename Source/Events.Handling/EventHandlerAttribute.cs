5ca30fd9d1e00682d0be2badf4783caed163c0c4
Magne Helleborg <magne.helleborg@gmail.com>
2023-06-21 10:35:57 +0200

    Disable interface for startFrom / stopAt, since it is not yet supported in the runtime release

--- Source/Events.Handling/EventHandlerAttribute.cs
+++ Source/Events.Handling/EventHandlerAttribute.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Globalization;
 using Dolittle.SDK.Common.Model;
 
 namespace Dolittle.SDK.Events.Handling;
@@ -24,33 +23,35 @@ public class EventHandlerAttribute : Attribute, IDecoratedTypeDecorator<EventHan
     /// <param name="inScope">The scope that the event handler handles events in.</param>
     /// <param name="alias">The alias for the event handler.</param>
     /// <param name="concurrency">How many events can be processed simultaneously</param>
-    /// <param name="resetTo">Where to start processing if the event handler does not have state. Defaults to the first event in the log.</param>
-    /// <param name="startFrom">Determines a specific event timestamp to start at if set. Overrides resetTo when used.</param>
-    /// <param name="stopAt">Determines a specific event timestamp to stop processing at if set.</param>
+    // /// <param name="resetTo">Where to start processing if the event handler does not have state. Defaults to the first event in the log.</param>
+    // /// <param name="startFrom">Determines a specific event timestamp to start at if set. Overrides resetTo when used.</param>
+    // /// <param name="stopAt">Determines a specific event timestamp to stop processing at if set.</param>
     public EventHandlerAttribute(
         string eventHandlerId,
         bool partitioned = true,
         string? inScope = null,
         string? alias = null,
-        int concurrency = 1,
-        ProcessFrom resetTo = ProcessFrom.First,
-        string? startFrom = null,
-        string? stopAt = null)
+        int concurrency = 1
+        // ,
+        // ProcessFrom resetTo = ProcessFrom.First,
+        // string? startFrom = null,
+        // string? stopAt = null
+        )
     {
         _eventHandlerId = eventHandlerId;
         _alias = alias;
         Concurrency = concurrency;
-        ResetTo = resetTo;
-        StartFrom = startFrom;
-        StopAt = stopAt;
+        // ResetTo = resetTo;
+        // StartFrom = startFrom;
+        // StopAt = stopAt;
         Partitioned = partitioned;
         Scope = inScope ?? ScopeId.Default;
     }
 
     public int Concurrency { get; set; }
-    public ProcessFrom ResetTo { get; }
-    public string? StartFrom { get; }
-    public string? StopAt { get; }
+    // public ProcessFrom ResetTo { get; }
+    // public string? StartFrom { get; }
+    // public string? StopAt { get; }
 
     /// <summary>
     /// Gets a value indicating whether this event handler is partitioned.
@@ -68,15 +69,15 @@ public class EventHandlerAttribute : Attribute, IDecoratedTypeDecorator<EventHan
     {
         DateTimeOffset? startFrom = null;
         DateTimeOffset? stopAt = null;
-        if (!string.IsNullOrWhiteSpace(StartFrom))
-        {
-            startFrom = DateTimeOffset.Parse(StartFrom, DateTimeFormatInfo.InvariantInfo);
-        }
-        if (!string.IsNullOrWhiteSpace(StopAt))
-        {
-            stopAt = DateTimeOffset.Parse(StopAt, DateTimeFormatInfo.InvariantInfo);
-        }
+        // if (!string.IsNullOrWhiteSpace(StartFrom))
+        // {
+        //     startFrom = DateTimeOffset.Parse(StartFrom, DateTimeFormatInfo.InvariantInfo);
+        // }
+        // if (!string.IsNullOrWhiteSpace(StopAt))
+        // {
+        //     stopAt = DateTimeOffset.Parse(StopAt, DateTimeFormatInfo.InvariantInfo);
+        // }
 
-        return new(_eventHandlerId, Partitioned, Scope, _alias ?? decoratedType.Name, Concurrency, ResetTo, startFrom, stopAt);
+        return new(_eventHandlerId, Partitioned, Scope, _alias ?? decoratedType.Name, Concurrency, ProcessFrom.First, startFrom, stopAt);
     }
 }
