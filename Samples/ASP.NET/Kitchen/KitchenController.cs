08893399343ca9d2fb50084ee723e1359810d59d
Magne Helleborg <magne.helleborg@gmail.com>
2024-03-12 17:44:12 +0100

    Cleanup

--- Samples/ASP.NET/Kitchen/KitchenController.cs
+++ Samples/ASP.NET/Kitchen/KitchenController.cs
@@ -1,11 +1,8 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Dolittle.SDK.Aggregates;
-using Dolittle.SDK.Projections.Store;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kitchen;
@@ -14,33 +11,10 @@ namespace Kitchen;
 public class KitchenController : ControllerBase
 {
     readonly IAggregateOf<Kitchen> _kitchen;
-    private readonly IProjectionOf<Pantry> _pantryClient;
 
-    public KitchenController(IAggregateOf<Kitchen> kitchen, IProjectionOf<Pantry> pantryClient)
+    public KitchenController(IAggregateOf<Kitchen> kitchen)
     {
         _kitchen = kitchen;
-        _pantryClient = pantryClient;
-    }
-
-    [HttpGet("{kitchen}/pantry")]
-    public async Task<IActionResult> Get([FromRoute] string kitchen)
-    {
-        var pantry = await _pantryClient
-            .Get(kitchen)
-            .ConfigureAwait(false);
-
-        return pantry is null ? NotFound() : Ok(pantry);
-    }
-
-    [HttpGet("{kitchen}/pantry-updates")]
-    public async Task<IActionResult> GetSomeUpdates([FromRoute] string kitchen, CancellationToken cancellationToken)
-    {
-        // using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-
-        using var updates = _pantryClient
-            .Subscribe<Pantry>(kitchen, cancellationToken);
-
-        return Ok(updates.Channel.ReadAllAsync(cancellationToken).ToBlockingEnumerable().Take(2).ToList());
     }
 
     [HttpPost("prepare")]
