1166fd9b166a251d85c796b740e07d046f713854
Magne Helleborg <magne.helleborg@gmail.com>
2024-10-23 08:43:10 +0200

    Redaction example usage

--- Samples/ASP.NET/Customers/Customer.cs
+++ Samples/ASP.NET/Customers/Customer.cs
@@ -1,15 +1,23 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using Dolittle.SDK.Aggregates;
 using Dolittle.SDK.Events;
+using Dolittle.SDK.Events.Redaction;
 using Microsoft.Extensions.Logging;
 
 namespace Customers;
+
 [AggregateRoot("dcdaecc0-29c9-41f4-96d1-9bddefe8b39a")]
 public class Customer : AggregateRoot
 {
     readonly ILogger<Customer> _logger;
+
+    string? _name;
+    string? _email;
+    string? _phoneNumber;
+    Address? _customerAddress;
 
     public Customer(EventSourceId eventSource, ILogger<Customer> logger)
         : base(eventSource)
@@ -19,9 +27,59 @@ public class Customer : AggregateRoot
 
     string Name => EventSourceId;
 
+    public void Register(string name, string email, string phoneNumber, Address address)
+    {
+        if (_name is not null)
+        {
+            throw new CustomerAlreadyRegistered();
+        }
+
+        Apply(new CustomerRegistered
+        {
+            Name = name,
+            Email = email,
+            PhoneNumber = phoneNumber,
+            CustomerAddress = address
+        });
+    }
+
+    public void GdprForget(string reason, string performedBy)
+    {
+        if (_name is null)
+        {
+            throw new CustomerNotRegistered();
+        }
+        
+
+        Apply(Redactions.Create<CustomerRegistered, CustomerDetailsForgotten>(reason, performedBy));
+    }
+
     public void EatDish(string dish)
     {
         Apply(new DishEaten(dish, Name));
         _logger.LogInformation("Customer {Name} is eating {Dish}", Name, dish);
     }
+
+    public void On(CustomerRegistered evt)
+    {
+        _name = evt.Name;
+        _email = evt.Email;
+        _phoneNumber = evt.PhoneNumber;
+        _customerAddress = evt.CustomerAddress;
+    }
+    
+    public void On(CustomerDetailsForgotten _)
+    {
+        _email = null;
+        _phoneNumber = null;
+        _customerAddress = null;
+    }
+}
+
+public class CustomerNotRegistered : Exception
+{
+}
+
+public class CustomerAlreadyRegistered : Exception
+{
 }
---
+++ Samples/ASP.NET/Customers/CustomerDetailsForgotten.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Dolittle.SDK.Events;
+using Dolittle.SDK.Events.Redaction;
+
+namespace Customers;
+
+[EventType("849494db-3173-4259-a4f7-e409ae1785dc")]
+public class CustomerDetailsForgotten : PersonalDataRedactedForEvent<CustomerRegistered>
+{
+    
+}
